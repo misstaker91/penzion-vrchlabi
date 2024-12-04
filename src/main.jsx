fea28def6b040e2e17981984fae021539e8b88ab
misstaker91 <petrik.janyk@gmail.com>
2024-12-04 13:54:56 +0100

    Fix: Move Firebase config to src directory

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import '../firebase/config.js';
+import './firebase/config.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
