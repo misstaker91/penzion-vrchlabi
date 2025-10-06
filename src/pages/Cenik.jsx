b108aecf9474e93bac61b7051911870f6ade70aa
misstaker91 <petrik.janyk@gmail.com>
2025-10-06 20:14:24 +0200

    Update Cenik2.jsx

--- src/pages/Cenik.jsx
+++ src/pages/Cenik.jsx
@@ -9,7 +9,7 @@ function Cenik() {
       <div className="price-info">
         <div className="price-section">
           <div className="price-table-container">
-            <h3>Zima 2024/2025 Svátky</h3>
+            <h3>Zima 2025/2026 Svátky</h3>
             <table className="price-table">
               <thead>
                 <tr>
@@ -21,55 +21,25 @@ function Cenik() {
               <tbody>
                 <tr>
                   <td>Apartmán se soc. zař.</td>
-                  <td>620</td>
-                  <td>590</td>
+                  <td>630</td>
+                  <td>600</td>
                 </tr>
                 <tr>
                   <td>Pokoj bez soc. zař. pro 2-4 osoby</td>
-                  <td>590</td>
-                  <td>560</td>
-                </tr>
-                <tr>
-                  <td>Pokoj bez soc. zař. pro 6-8 osob</td>
-                  <td>560</td>
-                  <td>530</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-
-          <div className="price-table-container">
-            <h3>Zima 2024/2025</h3>
-            <table className="price-table">
-              <thead>
-                <tr>
-                  <th></th>
-                  <th>Dospělý | Kč | den</th>
-                  <th>Dítě | Kč | den</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Apartmán se soc. zař.</td>
                   <td>600</td>
                   <td>570</td>
                 </tr>
                 <tr>
-                  <td>Pokoj bez soc. zař. pro 2-4 osoby</td>
+                  <td>Pokoj bez soc. zař. pro 6-8 osob</td>
                   <td>570</td>
                   <td>540</td>
-                </tr>
-                <tr>
-                  <td>Pokoj bez soc. zař. pro 6-8 osob</td>
-                  <td>540</td>
-                  <td>510</td>
                 </tr>
               </tbody>
             </table>
           </div>
 
           <div className="price-table-container">
-            <h3>Léto 2025</h3>
+            <h3>Zima 2025/2026</h3>
             <table className="price-table">
               <thead>
                 <tr>
@@ -81,18 +51,48 @@ function Cenik() {
               <tbody>
                 <tr>
                   <td>Apartmán se soc. zař.</td>
-                  <td>570</td>
-                  <td>540</td>
+                  <td>610</td>
+                  <td>580</td>
                 </tr>
                 <tr>
                   <td>Pokoj bez soc. zař. pro 2-4 osoby</td>
-                  <td>540</td>
-                  <td>510</td>
+                  <td>580</td>
+                  <td>550</td>
                 </tr>
                 <tr>
                   <td>Pokoj bez soc. zař. pro 6-8 osob</td>
-                  <td>510</td>
-                  <td>480</td>
+                  <td>550</td>
+                  <td>520</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+
+          <div className="price-table-container">
+            <h3>Léto 2026</h3>
+            <table className="price-table">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Dospělý | Kč | den</th>
+                  <th>Dítě | Kč | den</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Apartmán se soc. zař.</td>
+                  <td>580</td>
+                  <td>550</td>
+                </tr>
+                <tr>
+                  <td>Pokoj bez soc. zař. pro 2-4 osoby</td>
+                  <td>550</td>
+                  <td>520</td>
+                </tr>
+                <tr>
+                  <td>Pokoj bez soc. zař. pro 6-8 osob</td>
+                  <td>520</td>
+                  <td>490</td>
                 </tr>
               </tbody>
             </table>
